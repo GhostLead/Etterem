1bf199de4a01408cbda6183fad811bb35063f73d
GhostLead <72067053+GhostLead@users.noreply.github.com>
2022-11-10 01:53:52 +0100

    Végre kész van

---
+++ Etterem-vegleges/kosar.js
@@ -0,0 +1,296 @@
+
+var osszeg = 0;
+
+
+function pepsi_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_pepsi").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("uditok").style.color = "rgb(17, 84, 209)"; 
+}
+
+function viz_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_viz").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("uditok").style.color = "rgb(17, 84, 209)";
+}
+
+function limi_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_limi").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("uditok").style.color = "rgb(17, 84, 209)";
+}
+function toma_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_toma").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("uditok").style.color = "rgb(17, 84, 209)";
+}
+
+function szoda_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_szoda").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("uditok").style.color = "rgb(17, 84, 209)";
+}
+
+function kubu_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_kubu").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("uditok").style.color = "rgb(17, 84, 209)";
+}
+
+function kobanyai_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_kobanyai").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("sorok").style.color = "rgb(17, 84, 209)";
+}
+
+function soproni_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_soproni").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("sorok").style.color = "rgb(17, 84, 209)";
+}
+
+function csapolt_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_csapoltsor").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("sorok").style.color = "rgb(17, 84, 209)";
+}
+
+function egri_edes_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_egri_edes").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("sorok").style.color = "rgb(17, 84, 209)";
+}
+
+function egri_szaraz_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_egri_szaraz").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("sorok").style.color = "rgb(17, 84, 209)";
+}
+function egri_feledes_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_egri_feledes").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("sorok").style.color = "rgb(17, 84, 209)";
+}
+
+/*------------------------------------------------------------------------------------------------------------------------------------------*/
+
+function buri_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_buri").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("leves").style.color = "rgb(17, 84, 209)";
+}
+
+function frank_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_frank").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("leves").style.color = "rgb(17, 84, 209)";
+}
+
+function gulyas_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_gulyas").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("leves").style.color = "rgb(17, 84, 209)";
+}
+function gyumi_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_gyumi").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("leves").style.color = "rgb(17, 84, 209)";
+}
+
+function jogh_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_jogh").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("leves").style.color = "rgb(17, 84, 209)";
+}
+
+function brokkoli_k_kosarba() {
+    var ar = parseInt(document.getElementById("ar_brokkoli_k").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("leves").style.color = "rgb(17, 84, 209)";
+}
+
+function brassoi_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_brassoi").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("foetel").style.color = "rgb(17, 84, 209)";
+}
+
+function paprikas_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_paprikas").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("foetel").style.color = "rgb(17, 84, 209)";
+}
+
+function pizza_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_pizza").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("foetel").style.color = "rgb(17, 84, 209)";
+}
+function pori_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_pori").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("foetel").style.color = "rgb(17, 84, 209)";
+}
+
+function slambuc_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_slambuc").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("foetel").style.color = "rgb(17, 84, 209)";
+}
+
+function toltott_k_kosarba() {
+    var ar = parseInt(document.getElementById("ar_toltott_k").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("foetel").style.color = "rgb(17, 84, 209)";
+}
+
+function pite_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_pite").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("desszert").style.color = "rgb(17, 84, 209)";
+}
+
+function csoki_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_csoki").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("desszert").style.color = "rgb(17, 84, 209)";
+}
+
+function poharkrem_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_poharkrem").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("desszert").style.color = "rgb(17, 84, 209)";
+}
+function lepeny_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_lepeny").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("desszert").style.color = "rgb(17, 84, 209)";
+}
+
+function tekercs_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_tekercs").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("desszert").style.color = "rgb(17, 84, 209)";
+}
+
+function tiramisu_kosarba() {
+
+    var ar = parseInt(document.getElementById("ar_tiramisu").innerHTML);
+	osszeg += ar;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+    document.getElementById("desszert").style.color = "rgb(17, 84, 209)";
+}
+
+/*------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+function nullaz_etlap() {
+    osszeg = 0;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+
+    document.getElementById("leves").style.color = "white";
+    document.getElementById("foetel").style.color = "white";
+    document.getElementById("desszert").style.color = "white";
+}
+
+function nullaz_itallap() {
+    osszeg = 0;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+
+    document.getElementById("uditok").style.color = "white";
+    document.getElementById("sorok").style.color = "white";
+}
+
+function nullaz_fooldal() {
+    osszeg = 0;
+    let text_osszeg = osszeg.toString()
+    document.getElementById("szam").innerHTML = text_osszeg; 
+}
